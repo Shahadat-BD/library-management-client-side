09a56505518945f00ca059f336540328f8a26724
Shahadat-BD <shahadatstudent34@gmail.com>
2023-11-05 07:54:50 -0800

    specific user only display his/her borrowed books implement done

--- src/Pages/BorrowedBooks/BorrowedBooks.jsx
+++ src/Pages/BorrowedBooks/BorrowedBooks.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
+import BorrowedTable from '../BorrowedTable/BorrowedTable';
 
 const BorrowedBooks = () => {
+    const {user} = useContext(AuthContext)
+    const [borrowedBooks,setBorrowedBooks] = useState([])
+    useEffect(()=>{
+        fetch(`http://localhost:3000/borrowd-books?email=${user?.email}`)
+        .then(res => res.json())
+        .then(data => {
+            setBorrowedBooks(data)
+        })
+    },[])
     return (
-        <div>
-            <h1>This is borrowedBooks section</h1>
+            <div className='lg:w-[85%] m-auto w-[95%] mt-5 mb-10'>
+            <div className="overflow-x-auto mb-10">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th className='text-black font-bold'>Image</th>
+                            <th className='text-black font-bold'>Name</th>
+                            <th className='text-black font-bold'>Borrowed Date</th>
+                            <th className='text-black font-bold'>Category</th>
+                            <th className='text-black font-bold'>Return Date</th>
+                            <th className='text-black font-bold'>Borrowed Return</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                             borrowedBooks.map(borrowed => <BorrowedTable key={borrowed._id} borrowed={borrowed} ></BorrowedTable>)
+                        }
+                    </tbody>
+
+
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/BorrowedTable/BorrowedTable.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function BorrowedTable({borrowed}) {
+    const {bookImage,bookName,category,borrowedDate,returnDate} = borrowed
+    return (
+        <tr>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask rounded-md lg:w-32 lg:h-28 w-20 h-16">
+                <img src={bookImage} alt="service image" />
+              </div>
+            </div>
+          </div>
+        </td>
+        <td>
+            <div>
+                <div className="font-bold"> {bookName}</div>
+            </div>
+        </td>
+        <td>{borrowedDate}</td>
+        <td>{category}</td>
+        <td>{returnDate}</td>
+        <th>
+          <button className="text-white bg-pink-500 px-4 py-1 rounded-md">Return</button>
+        </th>
+      </tr>
+    )
+}
+
+export default BorrowedTable
