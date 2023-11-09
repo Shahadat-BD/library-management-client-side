8cbe1f5a88451598c465fb82d088863669329562
Shahadat-BD <shahadatstudent34@gmail.com>
2023-11-09 05:15:30 -0800

    updated design of notFound route and responsive for tablel user

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -10,7 +10,7 @@ const AboutUs = () => {
         </div>
         <div className="lg:w-1/2 w-full">
           <h3 className="text-pink-500 font-semibold text-md">About Us</h3>
-          <h5 className="lg:text-4xl text-2xl dark:text-white font-bold my-3">We are qualified &  experience in this filed.how to do library management system.</h5>
+          <h5 className="lg:text-4xl text-2xl dark:text-white font-bold my-3">We are qualified and experience in this filed. how to do library management system.</h5>
           <p className="text-[#737373] dark:text-[#ffffffa1] font-normal lg:w-[90%]">
             
           At BookStack, we are passionate about literature and knowledge. As your trusted partner in the world of books, we specialize in curating an extensive collection of novels, Islamic texts, programming guides, and business-related publications. Our mission is to connect readers with their next great read, offering a diverse range of books to satisfy every literary appetite.
